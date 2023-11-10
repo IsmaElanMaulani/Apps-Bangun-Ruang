f0e8e529ae787cb021677a0b0cead36500f5214e
Elan_Uln <107916522+Ismaelanmaulani@users.noreply.github.com>
2023-11-10 22:50:16 +0700

    Update Bola.py.

--- Aplikasi Bangun Ruang/Bola/Bola.py.py
+++ Aplikasi Bangun Ruang/Bola/Bola.py.py
@@ -2,19 +2,17 @@ import tkinter as tk
 from tkinter import Frame,Label,Entry,Button,END,W
 
 def hitung_luas():
-    p = float((txtpanjang.get()))
-    l = float((txtlebar.get()))
-    t = float((txttinggi.get()))
-    L = round((2 * p * l) + (2  * p * t) + (2* l * t))
+    phi = 3.14
+    r = float(txtjarijari.get())
+    L = round((4 * phi * r**2),2)
 
     txtLuas.delete(0, END)
     txtLuas.insert(END, L)
 
 def hitung_volume():
-    p = float((txtpanjang.get()))
-    l = float((txtlebar.get()))
-    t = float((txttinggi.get()))
-    V = round(p * l * t)
+    phi = 3.14
+    r = float(txtjarijari.get())
+    V = round((4/3 * phi * r**3),2)
 
     txtVolume.delete(0, END)
     txtVolume.insert(END, V)
@@ -27,49 +25,35 @@ def hitung():
 app = tk.Tk()
 
 # Judul
-app.title("Kalkulator Luas dan Volume Balok")
+app.title("Kalkulator Luas dan Volume Bola")
 
 # Windows
 frame = Frame(app)
 frame.pack (padx=20, pady=20)
 
-# Label Panjang
-panjang = Label(frame, text="Panjang:")
-panjang.grid(row=0, column=0, sticky=W, padx=5, pady=5)
-# Textbox Panjang
-txtpanjang = Entry(frame)
-txtpanjang.grid(row=0, column=1)
-
-# Label Lebar
-lebar = Label(frame, text="Lebar:")
-lebar.grid(row=1, column=0, sticky=W, padx=5, pady=5)
-# Textbox Lebar
-txtlebar = Entry(frame)
-txtlebar.grid(row=1, column=1)
-
-# Label Tinggi
-tinggi = Label(frame, text="Tinggi:")
-tinggi.grid(row=2, column=0, sticky=W, padx=5, pady=5)
-# Textbox Tinggi
-txttinggi = Entry(frame)
-txttinggi.grid(row=2, column=1)
+# Label Jari-Jari
+jarijari = Label(frame, text="Jari-Jari:")
+jarijari.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+# Textbox Jari-Jari
+txtjarijari = Entry(frame)
+txtjarijari.grid(row=0, column=1)
 
 # Button
 hitung_button = Button(frame, text="Hitung", command=hitung)
-hitung_button.grid(row=3, column=1, sticky=W, padx=5, pady=5)
+hitung_button.grid(row=1, column=1, sticky=W, padx=5, pady=5)
 
 # Output Label Luas
 luas= Label(frame, text="Luas:" )
-luas.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+luas.grid(row=2, column=0, sticky=W, padx=5, pady=5)
 # Textbox Luas
 txtLuas = Entry(frame)
-txtLuas.grid(row=4, column=1)
+txtLuas.grid(row=2, column=1)
 
 # Output Volume
 volume= Label(frame, text="Volume:" )
-volume.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+volume.grid(row=3, column=0, sticky=W, padx=5, pady=5)
 #  Textbox Volume 
 txtVolume = Entry(frame)
-txtVolume.grid(row=5, column=1)
+txtVolume.grid(row=3, column=1)
 
 app.mainloop()
